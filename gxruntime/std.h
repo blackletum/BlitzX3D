c3737c88521be964571e56ef4467220ec2d0f609
Chris A <cv565942@gmail.com>
2026-04-10 17:43:52 -0500

    Semi-functional OpenAL audio support
    fmod is completely purged, some audio fails to load sometimes

--- gxruntime/std.h
+++ gxruntime/std.h
@@ -5,7 +5,13 @@
 #define WIN32_LEAN_AND_MEAN
 #define POINTER_64 __ptr64
 
-#include "..//fmod375/include/fmod.h"
+// OpenAL headers
+#include "../openAL/include/AL/al.h"
+#include "../openAL/include/AL/alc.h"
+#include "../openAL/include/dr_wav.h"
+#include "../openAL/include/dr_mp3.h"
+
+#include "../fmod375/include/fmod.h"
 
 #include "../config/config.h"
 #include "../stdutil/stdutil.h"
